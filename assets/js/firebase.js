b5a08c598540c1c892fe80ee8172837d2241d44f
John N <43926285+jcniedfeldt@users.noreply.github.com>
2018-12-10 20:08:43 -0700

    Reads from database

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,4 +1,4 @@
-let DEBUG = false;
+
 
 satAPIbaseURL = "https://www.n2yo.com/rest/v1/satellite";
 apiKey = "YF4AW4-297NVD-YXBUEQ-3X95";
@@ -95,5 +95,368 @@ function updateSatInfo() {
 
 }
 
+function writeSatInfo2db() {
+  //load the satellite data from info.js
+  // $.getScript("../javascript/info.js");
+  const satellites = [{
+    name: "GGSE 1",
+    intDesignation: "1964-001B",
+    date: "November 1, 1964",
+    country: "United States"
+  },
+  {
+    name: "LES 1",
+    intDesignation: "1965-008C",
+    date: "February 11, 1965",
+    country: "United States"
+  },
+  {
+    name: "GGSE 2",
+    intDesignation: "1965-016B",
+    date: "March 9, 1965",
+    country: "United States"
+  },
+  {
+    name: "GGSE 3",
+    intDesignation: "1965-016C",
+    date: "March 9, 1965",
+    country: "United States"
+  },
+  {
+    name: "LES 2",
+    intDesignation: "1965-034B",
+    date: "May 6, 1965",
+    country: "United States"
+  },
+  {
+    name: "Asterix",
+    intDesignation: "1965-096A",
+    date: "November 26, 1965",
+    country: "France"
+  },
+  {
+    name: "ATS 1",
+    intDesignation: "1966-110A",
+    date: "December 7, 1966",
+    country: "United States"
+  },
+  {
+    name: "GGSE 4",
+    intDesignation: "1967-053C",
+    date: "May 31, 1967",
+    country: "United States"
+  },
+  {
+    name: "GGSE 5",
+    intDesignation: "1967-053D",
+    date: "May 31, 1967",
+    country: "United States"
+  },
+  {
+    name: "ATS 3",
+    intDesignation: "1967-111A",
+    date: "November 5, 1967",
+    country: "United States",
+    // fact:"Took the first color photo of the Earth."
+  },
+  {
+    name: "TOPO 1",
+    intDesignation: "1970-025B",
+    date: "April 8, 1970",
+    country: "United States"
+  },
+  {
+    name: "TANSEI 1",
+    intDesignation: "1971-011A",
+    date: "February 16, 1971",
+    country: "Japan"
+  },
+  {
+    name: "PROSPERO (BLACK ARROW)",
+    intDesignation: "1971-093A",
+    date: "October 28, 1971",
+    country: "United Kingdom"
+  },
+  {
+    name: "OPS 7518 (NTS 1)",
+    intDesignation: "1974-054A",
+    date: "July 14, 1971",
+    country: "United States"
+  },
+  {
+    name: "KIKU 1 (ETS 1)",
+    intDesignation: "1975-082A",
+    date: "September 9, 1975",
+    country: "Japan"
+  },
+  {
+    name: "TANSEI 3 (MS-T3)",
+    intDesignation: "1977-012A",
+    date: "February 19, 1977",
+    country: "Japan"
+  },
+  {
+    name: "NTS 2",
+    intDesignation: "1977-053A",
+    date: "June 23, 1977",
+    country: "United States"
+  },
+  {
+    name: "NTS 2",
+    intDesignation: "1977-053A",
+    date: "June 23, 1977",
+    country: "United States"
+  },
+  {
+    name: "AYAME 1 (ECS 1)",
+    intDesignation: "1979-009A",
+    date: "February 6, 1979",
+    country: "Japan"
+  },
+  {
+    name: "AYAME 2 (ECS 2)",
+    intDesignation: "1980-018A",
+    date: "February 22, 1980",
+    country: "Japan"
+  },
+  {
+    name: "KIKU 3 (ETS 4)",
+    intDesignation: "1981-012A",
+    date: "February 11, 1981",
+    country: "Japan"
+  },
+  {
+    name: "KIKU 4 (ETS 3)",
+    intDesignation: "1982-087A",
+    date: "September 3, 1982",
+    country: "Japan"
+  },
+  {
+    name: "DEBUT (ORIZURU)",
+    intDesignation: "1990-013B",
+    date: "February 7, 1990",
+    country: "Japan"
+  },
+  {
+    name: "OXP 1",
+    intDesignation: "1993-009A",
+    date: "February 9, 1993",
+    country: "United States"
+  },
+  {
+    name: "MAQSAT H",
+    intDesignation: "1997-066A",
+    date: "October 30, 1997",
+    country: "Europe"
+  },
+  {
+    name: "MAQSAT B",
+    intDesignation: "1997-066B",
+    date: "October 30, 1997",
+    country: "Europe"
+  },
+  {
+    name: "TEAMSAT",
+    intDesignation: "1997-066C",
+    date: "October 30, 1997",
+    country: "Europe"
+  },
+  {
+    name: "COMETS",
+    intDesignation: "1998-011A",
+    date: "February 21, 1998",
+    country: "Japan"
+  },
+  {
+    name: "PAN SAT",
+    intDesignation: "1998-064B",
+    date: "October 29, 1998",
+    country: "United States"
+  },
+  {
+    name: "SJ-5",
+    intDesignation: "1999-025B",
+    date: "May 10, 1999",
+    country: "China"
+  },
+  {
+    name: "OPAL",
+    intDesignation: "2000-004C",
+    date: "January 27, 2000",
+    country: "United States"
+  },
+  {
+    name: "FALCONSAT",
+    intDesignation: "2000-004D",
+    date: "January 27, 2000",
+    country: "United States"
+  },
+  {
+    name: "ASUSAT",
+    intDesignation: "2000-004E",
+    date: "January 27, 2000",
+    country: "United States"
+  },
+  {
+    name: "PICOSAT 1&2 (TETHERED)",
+    intDesignation: "2000-004H",
+    date: "January 27, 2000",
+    country: "United States"
+  },
+  {
+    name: "PICOSAT 3",
+    intDesignation: "2000-004J",
+    date: "January 27, 2000",
+    country: "United States"
+  },
+  {
+    name: "PICOSAT 4",
+    intDesignation: "2000-004K",
+    date: "January 27, 2000",
+    country: "United States"
+  },
+  {
+    name: "PICOSAT 5",
+    intDesignation: "2000-004L",
+    date: "January 27, 2000",
+    country: "United States"
+  },
+  {
+    name: "PICOSAT 6",
+    intDesignation: "2000-004M",
+    date: "January 27, 2000",
+    country: "United States"
+  },
+  {
+    name: "SNAP 1",
+    intDesignation: "2000-033C",
+    date: "June 28, 2000",
+    country: "Great Britain",
+    // fact: "Built by students"
+  },
+  {
+    name: "PICOSAT 9",
+    intDesignation: "2001-043B",
+    date: "September 30, 2001",
+    country: "Great Britain"
+  },
+  {
+    name: "PROBA 1",
+    intDesignation: "2001-049B",
+    date: "October 22, 2001",
+    country: "Belgium"
+  },
+  {
+    name: "CUTE-1",
+    intDesignation: "2003-031E",
+    date: "June 30, 2003",
+    country: "Japan"
+  },
+  {
+    name: "CANX-1",
+    intDesignation: "2003-031H",
+    date: "June 30, 2003",
+    country: "Canada"
+    // fact: "Built by students"
+  },
+  {
+    name: "CUBESAT XI-IV",
+    intDesignation: "2003-031J",
+    date: "June 30, 2003",
+    country: "Japan"
+  },
+  {
+    name: "NAXING 1",
+    intDesignation: "2004-012B",
+    date: "April 18, 2004",
+    country: "China"
+  },
+  {
+    name: "RAPIDEYE 2",
+    intDesignation: "2008-040A",
+    date: "August 29, 2008",
+    country: "Germany"
+  },
+  {
+    name: "RAPIDEYE 5",
+    intDesignation: "2008-040B",
+    date: "August 29, 2008",
+    country: "Germany"
+  },
+  {
+    name: "RAPIDEYE 1",
+    intDesignation: "2008-040C",
+    date: "August 29, 2008",
+    country: "Germany"
+  },
+  {
+    name: "RAPIDEYE 3",
+    intDesignation: "2008-040D",
+    date: "August 29, 2008",
+    country: "Germany"
+  },
+  {
+    name: "RAPIDEYE 4",
+    intDesignation: "2008-040E",
+    date: "August 29, 2008",
+    country: "Germany"
+  },
+  {
+    name: "RAZAKSAT",
+    intDesignation: "2009-037A",
+    date: "July 14, 2009",
+    country: "Malaysia"
+  },
+  {
+    name: "DUBAISAT 1",
+    intDesignation: "2009-041B",
+    date: "July 29, 2009",
+    country: "United Arab Emirates"
+  },
+  {
+    name: "TISAT 1",
+    intDesignation: "2010-035E",
+    date: "July 12, 2010",
+    country: "Switzerland"
+  },
+  {
+    name: "QZS-1 (MICHIBIKI)",
+    intDesignation: "2010-045A",
+    date: "September 11, 2010",
+    country: "Japan"
+  },
+  {
+    name: "ZHEDA PIXING 1B",
+    intDesignation: "2010-047B",
+    date: "September 22, 2010",
+    country: "China"
+  },
+  {
+    name: "ZHEDA PIXING 1C",
+    intDesignation: "2010-047C",
+    date: "September 22, 2010",
+    country: "China"
+  }
+  ]
+  let batch = db.batch();
+
+  satellites.forEach(satellite => {
+    let satIntID = satellite.intDesignation;
+
+    let satelliteDoc = db.collection("satInfo").doc(satIntID);
+    batch.set(satelliteDoc, satellite, { merge: true });
+
+
+  });
+  //create batch write to put info in db
+  batch.commit().then(function () {
+    console.log("Finished committing to Firebase!");
+  }).catch(function (error) {
+    console.log("Transaction failed: ", error);
+  });
+}
+
+// Only uncomment this line if we are adding to the database
+// writeSatInfo2db();
 
 $(document).on("click", "#testbutton", updateSatInfo);
